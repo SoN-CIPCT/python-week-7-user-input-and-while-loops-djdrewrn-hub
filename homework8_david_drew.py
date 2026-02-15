f11f293f2168b5dedf556b66c4db8f6438b2be76
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-15 03:53:41 +0000

    code

--- homework8_david_drew.py
+++ homework8_david_drew.py
@@ -1,2 +1,17 @@
-# list pizza_orders
-print ("hello test")
+# list for pizza_orders
+pizza_orders = ["pepperoni", "cheese", "combo"]
+
+# list for finished_pizzas
+finished_pizzas = []
+
+# loop through pizza orders
+while pizza_orders:
+    pizza = pizza_orders.pop(0)
+    print("Your pizza pie is finished!")
+    
+# Move the pizza to finished_pizzas
+    finished_pizzas.append(pizza)
+
+# Print a message for each finished pizza
+for pizza in finished_pizzas:
+    print(f"The pizza {pizza} was made.")
